a4045be500c0658fd3c762d5c8c572489a9d9167
zayma1 <zaymakinsiona@gmail.com>
2024-06-24 15:13:46 -0300

    Added Spring files

---
+++ backend/src/main/java/com/project/EstudanteMais/EstudanteMaisApplication.java
@@ -0,0 +1,13 @@
+package com.project.EstudanteMais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudanteMaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudanteMaisApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/project/EstudanteMais/EstudanteMaisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.EstudanteMais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudanteMaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
