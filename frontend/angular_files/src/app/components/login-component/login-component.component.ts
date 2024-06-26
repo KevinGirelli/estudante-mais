e183597a80d088b9e62d6354f00c2777300f5f09
Kevin Girelli <kevingirelli.gg@gmail.com>
2024-06-26 13:53:41 -0300

    Login Service

--- frontend/angular_files/src/app/components/login-component/login-component.component.ts
+++ frontend/angular_files/src/app/components/login-component/login-component.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { LoginService } from '../../services/login.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-login-component',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   templateUrl: './login-component.component.html',
   styleUrl: './login-component.component.scss'
@@ -14,7 +17,23 @@ export class LoginComponentComponent {
   matricula!: String;
   password!: String;
 
+  constructor(private loginService: LoginService) {}
+
   login(){
-    
+    const dados = {
+      matricula: this.matricula,
+      password: this.password
+    }
+
+    this.loginService.login(dados).subscribe(
+      (res: any) => {
+        console.log('Login realizado com sucesso: ', res);
+        this.matricula = '';
+        this.password = '';
+      },
+      (err: any) => {
+        console.error('Erro ao efetuar o login: ', err)
+      }
+    )
   }
 }
---
+++ frontend/angular_files/src/app/services/login.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  login(login: any): Observable<any>{
+    const httpOptions = {headers: new HttpHeaders({
+      "Content-type": "application/json"
+    })}
+    return this.httpClient.post("http://localhost:8080/login", login, httpOptions);
+  }
+}
