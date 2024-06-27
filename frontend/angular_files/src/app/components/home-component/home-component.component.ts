621091efe3338c591cfbd076b4ed3a7991e2c782
Kevin Girelli <kevingirelli.gg@gmail.com>
2024-06-27 20:42:20 -0300

    Add Frontend Page Home

--- frontend/angular_files/src/app/components/home-component/home-component.component.ts
+++ frontend/angular_files/src/app/components/home-component/home-component.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { StepperModule } from 'primeng/stepper';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-home-component',
   standalone: true,
-  imports: [],
+  imports: [
+    StepperModule,
+    ButtonModule,
+    
+  ],
   templateUrl: './home-component.component.html',
   styleUrl: './home-component.component.scss'
 })
