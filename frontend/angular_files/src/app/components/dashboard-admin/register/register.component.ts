0353ce1dc8e1a45861871004ab17cc5198c21a01
Kevin Girelli <kevingirelli.gg@gmail.com>
2024-07-05 20:02:49 -0300

    Building Frontend for Student Registration

--- frontend/angular_files/src/app/components/dashboard-admin/register/register.component.ts
+++ frontend/angular_files/src/app/components/dashboard-admin/register/register.component.ts
@@ -1,19 +1,46 @@
 import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { StepperModule } from 'primeng/stepper';
 
 @Component({
   selector: 'app-register',
   standalone: true,
   imports: [
-    NgClass
+    StepperModule,
+    ButtonModule,
+    NgClass,
+    FormsModule
   ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+
+  studentFullname!: string;
+  studentEmail!: string;
+  studentPassword!: string;
+  studentCPF!: string;
+  studentAge!: Date;
+  classes!: string;
+
   isMenuOpen = false;
 
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  cadastrarAluno() {
+    const dados = {
+      studentFullname: this.studentFullname,
+      studentEmail: this.studentEmail,
+      studentPassword: this.studentPassword,
+      studentCPF: this.studentCPF,
+      studentAge: this.studentAge,
+      classes: this.classes
+    };
+
+    
   }
 }
